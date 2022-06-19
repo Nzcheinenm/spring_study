20ee17c9cc3c9d19b8fd48e0e871014cdea875f8
Dmitry <koliasam1@gmail.com>
2022-06-19 17:59:18 +0300

    Добавлена логика для /main

--- src/main/java/ru/t1/spting_study/demo/controllers/MainController.java
+++ src/main/java/ru/t1/spting_study/demo/controllers/MainController.java
@@ -5,18 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/main")
+@RequestMapping
 public class MainController {
 
     @GetMapping("/main")
-    public String index() {
-        return "index";
+    public String index(Model model){   //(@RequestParam(value = "name",required = false) String name) {
+        model.addAttribute("name","Ivan");
+        return "hello";
     }
 
     @PostMapping("/main")
-    public String products(Model model) {
+    public String products() {
         return "redirect:/products";
     }
 
